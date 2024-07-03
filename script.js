84e69049b1516e517569a16be944d8cd3840c563
Ahmed <ahmed.gamal.xv@gmail.com>
2024-07-04 00:13:45 +0300

    add stop zoom

--- script.js
+++ script.js
@@ -89,3 +89,25 @@ document.addEventListener("DOMContentLoaded", function() {
         gridContainer.appendChild(gridItem);
     });
 });
+
+
+// stop zoom
+
+document.addEventListener('touchmove', function(event) {
+    if (event.scale !== 1) {
+        event.preventDefault();
+    }
+}, { passive: false });
+
+
+window.addEventListener('wheel', function(event) {
+    if (event.ctrlKey) {
+        event.preventDefault();
+    }
+}, { passive: false });
+
+window.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && (event.key === '+' || event.key === '-' || event.key === '0')) {
+        event.preventDefault();
+    }
+});
